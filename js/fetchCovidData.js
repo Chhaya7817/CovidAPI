5cc1406ba8919deba072ea266dc0872fe1884e50
Chhaya7817 <chhaya.chaudhary.visitme.in91@gmail.com>
2022-07-01 21:59:31 +0530

    Project Completed

---
+++ js/fetchCovidData.js
@@ -0,0 +1,31 @@
+const api_url="https://api.covid19api.com/summary";
+function getApiData()
+{
+    fetch(api_url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      this.data = data;
+      displayData();
+    })
+    .catch((error) => console.log(error.message));
+    
+}
+var data;
+function displayData(){
+  for (let i of data.Countries)
+ {
+ if(i.Country=='India')
+ {
+ document.getElementById("cname").textContent=i.Country;
+ document.getElementById("date").textContent=new Date().toDateString();
+ document.getElementById("Tcases").textContent=i.TotalConfirmed;
+ document.getElementById("Tdeaths").textContent=i.TotalDeaths;
+ document.getElementById("Ncases").textContent=i.NewConfirmed;
+ document.getElementById("Ndeaths").textContent=i.NewDeaths;
+
+ }
+}
+}
+getApiData();
